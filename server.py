faff9b9a5afa8ae884a72bcec1e0507eab217b02
Giselle Muriel <gisellemuriel123@gmail.com>
2026-04-18 03:16:52 -0400

    Included basic information for the homepage and implemented the route in server.py

--- server.py
+++ server.py
@@ -3,3 +3,10 @@ from flask import Flask, render_template, request, jsonify
 
 # create an app instance
 app = Flask(__name__)
+
+# this is a base route
+@app.route('/')
+
+# this is the home method
+def hello_world():
+  return render_template('homepage.html')
